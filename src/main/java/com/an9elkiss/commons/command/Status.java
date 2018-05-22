44c2a66adec2ee40b1d223bbb8c99b8137e754d3
an9elkiss <an9elkiss@163.com>
2018-05-22 22:36:12 +0800

    init commit

---
+++ src/main/java/com/an9elkiss/commons/command/Status.java
@@ -0,0 +1,12 @@
+package com.an9elkiss.commons.command;
+
+public interface Status {
+
+	Integer SUCCESS_CODE = 200;
+	String SUCCESS_MESSAGE = "²Ù×÷³É¹¦";
+
+	Integer getCode();
+
+	String getMessage();
+
+}
